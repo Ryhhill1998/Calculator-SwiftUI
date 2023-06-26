84259d5fdbc23a460141a77f70239043cc7abcb6
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-26 19:20:52 +0100

    Initial Commit

---
+++ Calculator SwiftUI/Calculator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_SwiftUIApp.swift
+//  Calculator SwiftUI
+//
+//  Created by Ryan Henzell-Hill on 26/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
