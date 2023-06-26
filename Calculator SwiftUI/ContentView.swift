ff354d05d2c1e450ef08bd18cadb154cd30d7153
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-26 19:47:32 +0100

    Began creating UI

--- Calculator SwiftUI/ContentView.swift
+++ Calculator SwiftUI/ContentView.swift
@@ -9,18 +9,81 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.ignoresSafeArea()
+            
+            VStack {
+                // output display
+                HStack {
+                    Spacer()
+                    
+                    Text("0")
+                        .bold()
+                        .font(.system(size: 60))
+                        .foregroundColor(.white)
+                }.padding(.all)
+                
+                // buttons
+                VStack {
+                    HStack {
+                        ButtonView()
+                        ButtonView()
+                        ButtonView()
+                        ButtonView()
+                    }
+                    
+                    HStack {
+                        ButtonView()
+                        ButtonView()
+                        ButtonView()
+                        ButtonView()
+                    }
+                    
+                    HStack {
+                        ButtonView()
+                        ButtonView()
+                        ButtonView()
+                        ButtonView()
+                    }
+                    
+                    HStack {
+                        ButtonView()
+                        ButtonView()
+                        ButtonView()
+                        ButtonView()
+                    }
+                    
+                    HStack {
+                        ButtonView()
+                        ButtonView()
+                        ButtonView()
+                        ButtonView()
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+
+struct ButtonView: View {
+    var body: some View {
+        Button {
+            print("Button clicked")
+        } label: {
+            Text("AC")
+        }
+        .font(.system(size: 30))
+        .frame(width: 70.0, height: 70.0)
+        .background(Color.orange)
+        .foregroundColor(.white)
+        .cornerRadius(35)
     }
 }
